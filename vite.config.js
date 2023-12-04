c35436eebbbdfada934d2fa0f4742bf206331576
Mikhail <mixa.penkin@gmail.com>
2023-12-04 15:52:21 +0300

    add alias @ to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/vue-vk-test-task',
-	plugins: [vue()]
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
